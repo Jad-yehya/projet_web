b36fa2e4739f0b0d48470156f9f7b7ed8bec1b59
Jad <jadyehya@hotmail.com>
2022-04-28 22:38:21 +0200

    ""

--- src/NewTweet.js
+++ src/NewTweet.js
@@ -1,18 +1,29 @@
+import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import style from './stylesheet/NewTweet.module.css';
 
+const BaseURL = 'http://localhost:4000/tweet/';
 
 function NewTweet(props) {
 
     const [text, setText] = useState({title: '', content: ''});
 
-    useEffect(() => {
-        console.log(text)
-    }, [text]);
+    const handleChange = (event) => {
+        setText({...text, [event.target.name]: event.target.value});
+    }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post(BaseURL+"createTweet/", {title: text.title, content: text.content, userid: 3})
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
     
-
     return (
         <div className={style.tweet_box}>
             <div className={style.title}>
@@ -21,9 +32,13 @@ function NewTweet(props) {
             <div className={style.TweetContent}>
                 <textarea className={style.TweetContent} placeholder="Content" value={text.content} onChange={(e) => setText({...text, content: e.target.value})}/>
             </div>
+            <button className={style.TweetButton} onClick={
+                (e) => {
+                    handleSubmit(e);
+                }
+            }>Tweet</button>
         </div>
     )
-
 
 }
 
